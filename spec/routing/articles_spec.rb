c379c5ee01a46d47f0cab313d902f58b46b70306
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-12 20:28:35 +0900

    :sparkles: 21. Routing for article's show action

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -4,4 +4,9 @@ describe 'articles routes' do
   it 'should route to articles index' do
     expect(get '/articles').to route_to('articles#index')
   end
+
+  it 'should route to articles show' do
+    # id の指定は文字列でないといけない
+    expect(get '/articles/1').to route_to('articles#show', id: '1')
+  end
 end
