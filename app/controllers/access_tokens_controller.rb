4c15209fd0c13e4f541f95d9e5fd402b56160c05
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-22 08:00:43 +0900

    :sparkles: 57. Standard login - Controller preparation
    - 複数の認証に対応するための準備
    - 認証していない場合のテストを変更
    - 認証コントローラーの受け取る引数を変更

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -5,7 +5,8 @@ class AccessTokensController < ApplicationController
   def create
     # もしここでエラーが起きた場合は ApplicationController で宣言している下記のコードが実行されエラーが返される
     #   rescue_from UserAuthenticator::AuthenticationError, with: :authentiation_error
-    authenticator = UserAuthenticator.new(params[:code])
+    # params[:code] だと 他の認証のフローを簡単に追加できないためよくない（拡張性がない）
+    authenticator = UserAuthenticator.new(authentication_params)
     authenticator.perform
 
     # ユーザーのアクセストークンを json で返す
@@ -15,5 +16,12 @@ class AccessTokensController < ApplicationController
   # DELETE /logout
   def destroy
     current_user.access_token.destroy
+  end
+
+  private
+
+  def authentication_params
+    # ハッシュに変換してシンボル化する
+    params.permit(:code).to_h.symbolie_keys
   end
 end
