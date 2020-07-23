0f3eb5bdaba58f41ba37b7afd7c56f17ad5f71ad
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-23 17:49:26 +0900

    :sparkles: 61. Encrypted password support for the user
    - encrypted_password カラムを User モデルに追加
    - bcrypt の gem を追加
    - User モデルに password 関連の処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,23 @@
 class User < ApplicationRecord
+  include BCrypt
+
   validates :login, presence: true, uniqueness: true
   validates :provider, presence: true
 
   has_one :access_token, dependent: :destroy
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def password
+    # OAuth をサポートしているためパスワードが入っていない場合があるため、
+    # 空の時は何もしない処理が必要である
+    # BCrypt を使用しているので hash が入っていないとエラーになるため、
+    # 「if encrypted_password.present?」この処理が必要である
+    @password ||= Password.new(encrypted_password) if encrypted_password.present?
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.encrypted_password = @password
+  end
 end
