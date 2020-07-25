673fff9cdacd52d9f6732dcc0b869e0ceae55c6a
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-25 21:16:44 +0900

    :sparkles: 63. Standard Authentication flow: Access token preparation.
    - access_token のメソッドを親クラス移動する
    - Oauth と Standard のテストを 親クラスの方に実装

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -3,7 +3,7 @@
 class UserAuthenticator
   class AuthenticationError < StandardError; end
 
-  attr_reader :authenticator
+  attr_reader :authenticator, :access_token
 
   # login: nil などの引数を使う場合は引数がセットされるかどうかわからない時に使用すること
   # 渡ってくる値が確実に決まっているのなら Standard.new(login, password) のように固定で宣言すること
@@ -19,13 +19,21 @@ class UserAuthenticator
     # インスタンス変数にアクセスする際、 @ ありとなしがあったがなしでも大丈夫な
     # 場合は attr_reader を宣言しているものが対象になる
     authenticator.perform
+
+    set_access_token
   end
 
   def user
     authenticator.user
   end
 
-  def access_token
-    authenticator.access_token
+  private
+
+  def set_access_token
+    @access_token = if user.access_token.present?
+                      user.access_token
+                    else
+                      user.create_access_token
+                    end
   end
 end
--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 describe UserAuthenticator do
+  let(:user) { create :user, login: 'dodonki1223', password: 'secret' }
+
+  shared_examples_for 'authenticator' do
+    it 'should create and set user access token' do
+      expect(authenticator.authenticator).to receive(:perform).and_return(true)
+      # at_least については以下を……
+      # https://relishapp.com/rspec/rspec-mocks/docs/setting-constraints/receive-counts
+      expect(authenticator.authenticator).to receive(:user).at_least(:once).and_return(user)
+      expect { authenticator.perform }.to change{ AccessToken.count }.by(1)
+      expect(authenticator.access_token).to be_present
+    end
+  end
+
   context 'when initialized with code' do
     let(:authenticator) { described_class.new(code: 'sample') }
     let(:authenticator_class) { UserAuthenticator::Oauth }
@@ -11,6 +24,8 @@ describe UserAuthenticator do
         authenticator
       end
     end
+
+    it_behaves_like 'authenticator'
   end
 
   context 'when initialized with login & password' do
@@ -22,6 +37,8 @@ describe UserAuthenticator do
         expect(authenticator_class).to receive(:new).with('dodonki1223', 'secret')
         authenticator
       end
+
+      it_behaves_like 'authenticator'
     end
   end
 end
