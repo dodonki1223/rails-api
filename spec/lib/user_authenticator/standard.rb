290895e0f9e621e51af393e130fbb5611854ba0e
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-25 20:38:20 +0900

    :sparkles: 62. Standard login service - finding the e user
    - Standard の処理を一部実装
    - DBにユーザーデータが存在する時の処理を実装

--- spec/lib/user_authenticator/standard.rb
+++ spec/lib/user_authenticator/standard.rb
@@ -5,26 +5,39 @@ describe UserAuthenticator::Standard do
     let(:authenticator) { described_class.new('dodonki1223', 'password') }
     subject { authenticator.perform }
 
-    before { user }
-
     shared_examples_for 'invalid authentication' do
+      before { user }
+
       it 'should raise an error' do
-        expect{ subject }.to raise_errors(UserAuthenticator::Standard::AuthenticationError)
+        expect{ subject }.to raise_error(UserAuthenticator::Standard::AuthenticationError)
         expect(authenticator.user).to be_nil
       end
     end
 
     context 'when invalid login' do
-      let(:user) { create :user, login: 'dodonki1223', password: 'secret' }
+      let(:user) { create :user, login: 'ddoe', password: 'password' }
       it_behaves_like 'invalid authentication'
     end
 
     context 'when invalid password' do
+      let(:user) { create :user, login: 'dodonki1223', password: 'secret' }
       it_behaves_like 'invalid authentication'
     end
 
     context 'when successed auth' do
-      
+      let(:user) { create :user, login: 'dodonki1223', password: 'password' }
+
+      before { user }
+
+      it 'should set the user found in db' do
+        expect { subject }.not_to change{ User.count }
+        expect(authenticator.user).to eq(user)
+      end
+
+      it 'should create and set user access token' do
+        expect { subject }.to change{ AccessToken.count }.by(1)
+        expect(authenticator.access_token).to be_present
+      end
     end
   end
 end
